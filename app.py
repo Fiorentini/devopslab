3b41f48e9610be8ea96309654c6ca8a87e188c69
Kalebi Fiorentini <kalebi.oliveira@hotmail.com>
2022-05-19 22:55:09 -0300

    ajustando a mensagem

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "<h1>Teste de mensagem</h1>"
+    return "<center><h1>Teste de mensagem</h1></center>"
 
 if __name__ == '__main__':
     app.run()
