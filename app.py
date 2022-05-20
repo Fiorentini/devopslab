7afaeae13e4eb52506828ce9c2df8c9df3a9c5a3
Kalebi Fiorentini <kalebi.oliveira@hotmail.com>
2022-05-19 22:57:18 -0300

    Troca na mensagem

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "<center><h1>Teste de mensagem</h1></center>"
+    return "<center><h1>Te amo morin e to morrendo de saudades! ♥</h1></center>"
 
 if __name__ == '__main__':
     app.run()
